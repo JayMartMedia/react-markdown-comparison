bae41013addaf39179d434f629d49d31ec5f858c
JayMartMedia <Jay@JayMartMedia.com>
2022-03-25 20:12:31 -0400

    Added state for saving and passing text

--- src/markdown/Markdown.jsx
+++ src/markdown/Markdown.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Editor from './editor/Editor'
 import Preview from './preview/Preview'
 import styles from './Markdown.module.css'
 
 function Markdown () {
+  const [text, setText] = useState('')
   return (
     <div className={styles.container}>
       <div className={styles.editorContainer}>
-        <Editor />
+        <Editor
+          setText={setText}
+        />
       </div>
       <div className={styles.previewContainer}>
-        <Preview />
+        <Preview
+          text={text}
+        />
       </div>
     </div>
   )
